f6b05ecdc802d76aaacb7d15ad8875166236baf0
Zaid Iqbal <zish79@gmail.com>
2014-12-13 14:05:05 +0500

    FIrstPush

---
+++ AmazaonScrapping/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace AmazaonScrapping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1081, 180);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Fetch Amazon Data and Prices";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(390, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Seller ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(302, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(249, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Starting Page Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter Ending Page Number:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(25, 204);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(216, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Enter File Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 492);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AmazaonScrapping/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+
+
+
+namespace AmazaonScrapping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ioexception = 0;
+            int webexception = 0;
+            string MyFile = @"d:\"+textBox4.Text.ToString().Trim()+" "+textBox1.Text.ToString().Trim()+"-"+textBox3.Text.ToString().Trim()+".xls";
+            Microsoft.Office.Interop.Excel.Application xl = null;
+            Microsoft.Office.Interop.Excel._Workbook wb = null;
+            Microsoft.Office.Interop.Excel._Worksheet sheet = null;
+            //VBIDE.VBComponent module = null;
+            bool SaveChanges = false;
+
+            if (File.Exists(MyFile)) { File.Delete(MyFile); }
+
+
+            xl = new Microsoft.Office.Interop.Excel.Application();
+            xl.Visible = false;
+
+            wb = (Microsoft.Office.Interop.Excel._Workbook)(xl.Workbooks.Add(Missing.Value));
+            wb.Sheets.Add(Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            wb.Sheets.Add(Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            wb.Sheets.Add(Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            wb.Sheets.Add(Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            wb.Sheets.Add(Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+
+            sheet = (Microsoft.Office.Interop.Excel._Worksheet)(wb.Sheets[1]);
+
+            //for (int r = 0; r < 20; r++)
+            //{
+
+            //    for (int c = 0; c < 10; c++)
+            //    {
+            //        sheet.Cells[r + 1, c + 1] = 125;
+            //    }
+            //}
+
+
+
+
+
+            // Let loose control of the Excel instance
+
+
+
+            // Get the stream from the returned web response
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            string strLine;
+            // Read the stream a line at a time and place each one
+            // into the stringbuilder
+            int Pr_Count = 0;
+            int r = 2;
+            int c = 0;
+            int starter = 0;
+            int Ender = 0;
+            try
+            {
+                 starter = Convert.ToInt32(textBox1.Text.ToString());
+                 Ender = Convert.ToInt32(textBox3.Text.ToString());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Enter Valid Page Number");
+                return;
+            }
+            if (!string.IsNullOrEmpty(textBox2.Text.ToString().Trim()))
+            {
+
+
+                for (int ii = starter; ii <= Ender; ii++)
+                {
+                    Pr_Count++;
+                    if (Pr_Count >= 25)
+                    {
+                        MessageBox.Show("Buy A Premium Tool in Just 50$ to Fetch 100% Data");
+                        return;
+                    }
+                    sheet.Cells[r, c + 1] = "Page: "+ii.ToString();
+                    r++;
+                    string url1 = "http://www.amazon.com/gp/shops/storefront/index.html?ie=UTF8&isSearch=1&page=";
+                    string url2 = "&searchTerm=&sellerID=" + textBox2.Text.ToString().Trim() + "&sortBy=StartDateDesc";
+                    try
+                    {
+                        WebRequest req = WebRequest.Create(url1 + ii + url2);
+                        if (req != null)
+                        {
+
+
+                            if (req.GetResponse() != null)
+                            {
+                                // Get the stream from the returned web response
+
+                                StreamReader stream = new StreamReader(req.GetResponse().GetResponseStream());
+                                if (stream != null)
+                                {
+                                    while ((strLine = stream.ReadLine()) != null)
+                                    {
+                                        //// Ignore blank lines
+                                        //if (strLine.Length > 0)
+                                        //    sb.Append(strLine);
+                                        string sss = "";
+                                        if (strLine.Contains("<span class='small'><a href="))
+                                        {
+                                            int last = strLine.IndexOf("<span class='small'><a href=");
+                                            if (last != -1)
+                                            {
+                                                last = last + 41;
+                                                char[] array = strLine.ToCharArray();
+                                                while (array[last] != '/')
+                                                {
+                                                    sss += array[last];
+
+                                                    last++;
+                                                }
+                                                sheet.Cells[r + 1, c + 1] = sss;
+                                                r++;
+                                            }
+                                        }
+
+                                        //if (strLine.Contains("class=h3color"))
+                                        //{
+                                        //    if (strLine.Contains("No results found."))
+                                        //    {
+                                        //        MessageBox.Show("Enter Correct Seller ID");
+                                        //        return;
+                                        //    }
+                                            
+                                        //}
+
+                                        sss = "";
+                                        //if (strLine.Contains("<br>Buy New: <a href="))
+                                        //{
+                                        //    char[] array = strLine.ToCharArray();
+                                        //    int size = array.Length;
+                                        //    for (int i = 0; i < size - 1; i++)
+                                        //    {
+                                        //        if (array[i] == '>' && array[i + 1] == '$')
+                                        //        {
+                                        //            int k = i;
+                                        //            for (int j = k + 1; array[j] != '<'; j++, i++)
+                                        //            {
+                                        //                sb.Append(array[j]);
+
+                                        //                sss += array[j];
+
+                                        //            }
+                                        //            sheet.Cells[r + 1, c + 6] = sss;
+                                        //        }
+                                        //    }
+                                        //    r++;
+                                        //}
+
+                                        //if (strLine.Contains("Currently Unavailable"))
+                                        //{
+
+                                        //    sheet.Cells[r + 1, c + 6] = "Currently Unavailable";
+                                        //    r++;
+                                        //}
+
+                                    }
+
+
+                                }
+                                stream.Close();
+                            }
+
+                        }
+                    }
+                    catch (WebException)
+                    {
+                        webexception++;
+                        MessageBox.Show("Unable to Fetch Data from Server Against Page Number: "+ii.ToString()+"\n");
+                        MessageBox.Show("Sorry! Execution Stops Itself at this Page Number");
+                        sheet.Name = "Movie";
+                        sheet.Cells[1, 1] = "ASIN";
+                        xl.Visible = false;
+                        xl.UserControl = false;
+                        SaveChanges = true;
+
+                        wb.SaveAs(MyFile, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal,
+                                  null, null, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlShared,
+                                  false, false, null, null, null);
+                        xl.Visible = false;
+                        xl.UserControl = false;
+                        // Close the document and avoid user prompts to save if our method failed.
+                        wb.Close(SaveChanges, null, null);
+                        xl.Workbooks.Close();
+                        Dispose();
+                    }
+                    catch (IOException)
+                    {
+                        ioexception++;
+                        MessageBox.Show("Unable to Fetch Data from Server Against Page Number: "+ii.ToString()+"\n");
+                        MessageBox.Show("Sorry! Execution Stops Itself at this Page Number");
+                        sheet.Name = "Movie";
+                        sheet.Cells[1, 1] = "ASIN";
+
+                        xl.Visible = false;
+                        xl.UserControl = false;
+
+                        SaveChanges = true;
+
+
+                        wb.SaveAs(MyFile, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal,
+                                  null, null, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlShared,
+                                  false, false, null, null, null);
+                        xl.Visible = false;
+                        xl.UserControl = false;
+                        // Close the document and avoid user prompts to save if our method failed.
+                        wb.Close(SaveChanges, null, null);
+                        xl.Workbooks.Close();
+                        Dispose();
+                    }
+                    catch (Exception)
+                    {
+                        ioexception++;
+                        MessageBox.Show("Unable to Fetch Data from Server Against Page Number: " + ii.ToString() + "\n");
+                        MessageBox.Show("Sorry! Execution Stops Itself at this Page Number");
+                        sheet.Name = "Movie";
+                        sheet.Cells[1, 1] = "ASIN";
+
+                        xl.Visible = false;
+                        xl.UserControl = false;
+
+                        SaveChanges = true;
+
+
+                        wb.SaveAs(MyFile, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal,
+                                  null, null, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlShared,
+                                  false, false, null, null, null);
+                        xl.Visible = false;
+                        xl.UserControl = false;
+                        // Close the document and avoid user prompts to save if our method failed.
+                        wb.Close(SaveChanges, null, null);
+                        xl.Workbooks.Close();
+                        Dispose();
+                    }
+                    r = r + 2;
+                    
+
+                }
+            }
+
+            // Finished with the stream so close it now
+
+
+
+            // set come column heading names
+            sheet.Name = "Movie";
+            sheet.Cells[1, 1] = "ASIN";
+
+            xl.Visible = false;
+            xl.UserControl = false;
+
+            // Set a flag saying that all is well and it is ok to save our changes to a file.
+
+            SaveChanges = true;
+
+            //  Save the file to disk
+
+            wb.SaveAs(MyFile, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal,
+                      null, null, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlShared,
+                      false, false, null, null, null);
+            xl.Visible = false;
+            xl.UserControl = false;
+            // Close the document and avoid user prompts to save if our method failed.
+            wb.Close(SaveChanges, null, null);
+            xl.Workbooks.Close();
+            Dispose();
+        }
+    }
+}
